64e2577472dba4047d8ab5b7b75ed13c9964ffa4
Rajeevan <rajee.srs@gmail.com>
2023-05-28 09:08:14 -0700

    Videos Scss File

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,6 +1,12 @@
 import "./Videos.scss";
 
 function Videos(props) {
+
+  function scrollToTop (){
+    window.scrollTo({top:0, behavior: "smooth"})
+  }
+
+
   return (
     <section className="videos">
       <h2 className="videos__title">Next Videos</h2>
@@ -8,11 +14,13 @@ function Videos(props) {
       {props.videos
         .filter((video) => video.id !== props.selectedVideo.id)
         .map((video) => (
-          <div className="videos__box">
-            <div
-              className="videos__picture"
-              key={video.id}
-              onClick={() => props.handleVideoClick(video.id)}
+            <Link 
+            className="videos__box"
+            key={video.id}
+            to={`/videos/${video.id}`}
+            onClick = {scrollToTop}
+            >
+            <div className="videos__picture"
             >
               <img className="videos__image" src={video.image} />
             </div>
@@ -20,7 +28,7 @@ function Videos(props) {
               <p className="videos__name">{video.title}</p>
               <p className="videos__channel">{video.channel}</p>
             </div>
-          </div>
+            </Link>
         ))}
     </section>
   );
